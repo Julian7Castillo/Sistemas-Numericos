eb855bfc75bba2f9fdcd35a62425fbbda17cc8a7
Julian Castillo <oscarcastillo212002@gmail.com>
2024-02-20 23:17:41 -0500

    Se agregan las iumagenes para las seccion de figuras geometricas, y comienzo de contruccion de panel del cuadrado

--- src/forms/form_area_trabajo/form_figuras/figuras.py
+++ src/forms/form_area_trabajo/form_figuras/figuras.py
@@ -1,5 +1,45 @@
 import tkinter as tk
 
-def figurasGeometricas(self):
-    texto = tk.Label(self.AreaTrabajo, text="Figuras geometricas", font = ("Arial", 35, "bold"), bg="light grey")
-    texto.pack(side=tk.TOP, fill="both", expand=True)
+def figurasGeometricas(self, figura):
+    texto = tk.Label(self.AreaTrabajo, text="Figuras geometricas", font = ("Arial", 30, "bold"), bg="light grey")
+    texto.pack(side=tk.TOP, fill="x", expand=True)
+    
+    if(figura == 0):
+        #texto de la figura 
+        texfig = tk.Label(self.AreaTrabajo, text="Cuadrado", font = ("Arial", 20, "bold"), bg="light grey")
+        texfig.pack(side=tk.LEFT, fill="y", expand=True)
+        
+        #frame donde se mostrara la figura y los campos a llenar 
+        fig = tk.Frame(self.AreaTrabajo, bg="light grey")
+        fig.pack(side=tk.RIGHT, fill="both", expand=True)
+        
+        l1 = tk.Entry(fig, font = ("Arial", 20, "bold"), width=3)
+        l1.grid(column=0, row=0)
+        
+        cua = tk.Label(fig, image = self.img_Cuadrado, padx=50, pady=50)  
+        cua.grid(column=0, row=2)  
+        
+        l2 = tk.Entry(fig, font = ("Arial", 20, "bold"), width=3)   
+        l2.grid(column=1, row=2)
+        
+        #borde del boton 1
+        borderlineal = tk.LabelFrame(fig, bd = 6, bg = "DodgerBlue2")
+        borderlineal.grid(column=0, row=4)
+        calcular = tk.Button(borderlineal, text="Calcular" )
+        calcular.pack()
+    
+    elif(figura == 1):
+        texfig = tk.Label(self.AreaTrabajo, text="Triangulo", font = ("Arial", 20, "bold"), bg="light grey")
+        texfig.pack(side=tk.TOP, fill="x", expand=True)
+    
+    elif(figura == 2):
+        texfig = tk.Label(self.AreaTrabajo, text="Rectangulo", font = ("Arial", 20, "bold"), bg="light grey")
+        texfig.pack(side=tk.TOP, fill="x", expand=True)
+    
+    elif(figura == 3):
+        texfig = tk.Label(self.AreaTrabajo, text="Circulo", font = ("Arial", 20, "bold"), bg="light grey")
+        texfig.pack(side=tk.TOP, fill="x", expand=True)
+    
+    elif(figura == 4):
+        texfig = tk.Label(self.AreaTrabajo, text="Paralelogramo", font = ("Arial", 20, "bold"), bg="light grey")
+        texfig.pack(side=tk.TOP, fill="x", expand=True)
