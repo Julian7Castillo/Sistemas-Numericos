ddade8c450bac003f96293a4a459668b2a0b3655
Julian Castillo <oscarcastillo212002@gmail.com>
2024-02-24 23:47:13 -0500

    Ajuste del panel del error absolutoy relativo en el cual ya tenemsoi los campos necesrios para realizar las operaciones

--- src/forms/form_area_trabajo/form_error/error.py
+++ src/forms/form_area_trabajo/form_error/error.py
@@ -7,12 +7,28 @@ def errorabcoluto(self):
     # texto del tituo y de digitar el valor con un campo de texto para digitar valores
     tk.Label(self.AreaTrabajo, text="Error Absoluto y Relativo", font= ("Arial", 35, "bold"), justify="center", bg="light gray").pack(side=tk.TOP, expand=True)
     
-    tk.Label(self.AreaTrabajo, text="Digite el valor: ", font= ("Arial", 25, "bold"), justify="center", bg="light gray").pack(side=tk.TOP, expand=True)
+    tk.Label(self.AreaTrabajo, text="Valor Exacto: ", font= ("Arial", 20, "bold"), justify="center", bg="light gray").pack(side=tk.TOP, expand=True)
     
-    valor = tk.Entry(self.AreaTrabajo, font= ("Arial", 25))
-    valor.pack(side=tk.TOP, expand=True)
+    valEx = tk.Entry(self.AreaTrabajo, font= ("Arial", 20))
+    valEx.pack(side=tk.TOP, expand=True)
     
-    tk.Label(self.AreaTrabajo, text="Digite el valor: ", font= ("Arial", 25, "bold"), justify="center", bg="light gray").pack(side=tk.TOP, expand=True)
+    tk.Label(self.AreaTrabajo, text="Valor Aproximado: ", font= ("Arial", 20, "bold"), justify="center", bg="light gray").pack(side=tk.TOP, expand=True)
     
-    valor = tk.Entry(self.AreaTrabajo, font= ("Arial", 25))
-    valor.pack(side=tk.TOP, expand=True)
+    valApx = tk.Entry(self.AreaTrabajo, font= ("Arial", 20))
+    valApx.pack(side=tk.TOP, expand=True)
+    
+    tk.Label(self.AreaTrabajo, text="Error Absoluto: ", font= ("Arial", 20, "bold"), justify="center", bg="light gray").pack(side=tk.TOP, expand=True)
+    
+    errAb = tk.Entry(self.AreaTrabajo, font= ("Arial", 20))
+    errAb.pack(side=tk.TOP, expand=True)
+    
+    tk.Label(self.AreaTrabajo, text="Error Relativo: ", font= ("Arial", 20, "bold"), justify="center", bg="light gray").pack(side=tk.TOP, expand=True)
+    
+    errRl = tk.Entry(self.AreaTrabajo, font= ("Arial", 20))
+    errRl.pack(side=tk.TOP, expand=True)
+    
+    borderlineal = tk.LabelFrame(self.AreaTrabajo, bd = 6, bg = "DodgerBlue2")
+    borderlineal.pack(side=tk.TOP, expand=True)
+    calcular = tk.Button(borderlineal, text="Calcular", width=15, font = ("Arial", 15, "bold") )
+    calcular.pack()
+
